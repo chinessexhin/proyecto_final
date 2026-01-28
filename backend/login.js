435ff828741096ab6bdd08c67a6c394c7eb8822b
aidem <chinessexhin@gmail.com>
2026-01-28 02:48:10 -0300

    m

--- backend/login.js
+++ backend/login.js
@@ -7,18 +7,17 @@ router.post("/login", async (req, res) => {
   );
 
   if (result.rowCount === 0)
-    return res.status(401).json({ msg: "Usuario no existe" });
+    return res.status(401).json({ msg: "usuario no existe" });
 
   const user = result.rows[0];
 
   const valid = await bcrypt.compare(password, user.password);
 
   if (!valid)
-    return res.status(401).json({ msg: "Password incorrecta" });
+    return res.status(401).json({ msg: "contraseña incorrecta" });
 
   const token = jwt.sign(
     { id: user.id, rol: user.rol },
-    "SECRETO_SUPER_PRO",
     { expiresIn: "2h" }
   );
 
